0b2d92c6449b35a7329347b3b4d5ce03973bc89d
Michael Marino <mmarino@gmail.com>
2015-06-24 10:11:48 +0200

    Handle base-conf by replacing @REDIRECT_URI@ in string.  This is stabler and faster.

--- build-conf-files.py
+++ build-conf-files.py
@@ -7,8 +7,7 @@ _env_prefix = "NGX_VIRTUAL_SERVER"
 _conf_template = """
 server {{
   server_name {server_name};
-  {redirect_uri}
-  include {base_conf};
+  {base_conf}
 }}
 """
 
@@ -16,16 +15,15 @@ server {{
 shutil.rmtree(_nginx_conf, ignore_errors=True)
 
 os.makedirs(_nginx_conf)
+
+# Grab the base-conf file
+base_conf = open("/nginx-base.conf.in").read().replace("@DB_NAME@", os.environ["DB_PORT_5984_TCP_ADDR"])
 for nm,val in os.environ.items():
     if nm[:len(_env_prefix)] != _env_prefix: continue
     server_name, redirect = val.split('@')
-    test = "set $redirect_uri {redirect};".format(redirect=redirect)
-    if redirect == "" or redirect == "/": test = ""
+    redirect = base_conf.replace("@REDIRECT_URI@", redirect)
     with open(os.path.join(_nginx_conf, nm + ".conf"), "w") as af:
         af.write(
-          _conf_template.format(server_name=server_name,redirect_uri=test,base_conf=_base_conf)
+          _conf_template.format(server_name=server_name,base_conf=redirect)
         )
 
-# Write the nginx-base.conf file
-af = open("/nginx-base.conf.in").read()
-open(_base_conf, "w").write(af.replace("@DB_NAME@", os.environ["DB_PORT_5984_TCP_ADDR"]))
